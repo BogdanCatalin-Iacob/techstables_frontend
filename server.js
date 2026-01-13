9917c5495e49fc761eb6894fc0dc1323e986fba7
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2026-01-13 12:56:33 +0000

    Refactor fallback regex to match express 5 path to regex

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url"
-
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,11 +8,14 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from Vite's dist folder
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"))
+
+// SPA fallback
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
