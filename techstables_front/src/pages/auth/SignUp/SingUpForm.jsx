61d306f0501488af7d20455349f442fa2fb3b309
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2026-01-13 09:42:14 +0000

    Added password confirmation, link to sign in page, and basic styles for the form

--- techstables_front/src/pages/auth/SignUp/SingUpForm.jsx
+++ techstables_front/src/pages/auth/SignUp/SingUpForm.jsx
@@ -2,32 +2,38 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
+import { Link } from 'react-router'
 import styles from './SignUpForm.module.css'
 
 const SingUpForm = () => {
-    return (
-        <Container className={styles.wrapper}>
+    return (<>
+        <Container className={`${styles.wrapper} text-center`}>
+            <h1 className={styles.heading}>Sign Up</h1>
             <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+                {/* Email address input */}
+                <Form.Group className={styles.formGroup} controlId="email">
+                    <Form.Control className={styles.Input} type="email" name="email" placeholder="Email address" />
+                    <Form.Label className={styles.formLabel}>Email address</Form.Label>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                {/* Password input */}
+                <Form.Group className={styles.formGroup} controlId="password">
+                    <Form.Control className={styles.Input} type="password" name="password" placeholder="Password" />
+                    <Form.Label className={styles.formLabel}>Password</Form.Label>
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
+
+                {/* Password confirmation input */}
+                <Form.Group className="mb-3" controlId="confirm-password">
+                    <Form.Control className={styles.Input} type="password" name="confirm_password" placeholder="Confirm password" />
+                    <Form.Label className={styles.formLabel}>Confirm password</Form.Label>
                 </Form.Group>
                 <Button variant="primary" type="submit">
-                    Submit
+                    Register
                 </Button>
+                <Link to="/signin" className='d-block mt-3'>Already have an account? <span>Sign in</span></Link>
             </Form>
         </Container>
+    </>
     );
 }
 
