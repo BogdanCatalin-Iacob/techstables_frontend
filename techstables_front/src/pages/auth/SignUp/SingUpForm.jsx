28e42f67215fd4772f10a04be94a17097ea466f7
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2026-01-13 10:57:37 +0000

    Add state and  onchange attribute to the form for all inputs

--- techstables_front/src/pages/auth/SignUp/SingUpForm.jsx
+++ techstables_front/src/pages/auth/SignUp/SingUpForm.jsx
@@ -1,31 +1,68 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container'
-import Form from 'react-bootstrap/Form'
-import { Link } from 'react-router'
-import styles from './SignUpForm.module.css'
-import ButtonSubmit from '../../../components/Button/ButtonSubmit'
+import React, { useState } from 'react';
+import axios from 'axios'
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import { Link, useNavigate } from 'react-router';
+import styles from './SignUpForm.module.css';
+import ButtonSubmit from '../../../components/Button/ButtonSubmit';
 
 const SingUpForm = () => {
+
+    const [signUpData, setSignUpData] = useState({
+        email: '',
+        password: '',
+        confirm_password: ''
+    });
+    const { email, password, confirm_password } = signUpData;
+
+  
+
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value
+        })
+    };
+
+
+
     return (<>
         <Container className={`${styles.wrapper} text-center`}>
             <h1 className={styles.heading}>Sign Up</h1>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 {/* Email address input */}
                 <Form.Group className={styles.formGroup} controlId="email">
-                    <Form.Control className={styles.Input} type="email" name="email" placeholder="Email address" />
+                    <Form.Control
+                        className={styles.Input}
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                        placeholder="Email address" />
                     <Form.Label className={styles.formLabel}>Email address</Form.Label>
                 </Form.Group>
 
                 {/* Password input */}
                 <Form.Group className={styles.formGroup} controlId="password">
-                    <Form.Control className={styles.Input} type="password" name="password" placeholder="Password" />
+                    <Form.Control
+                        className={styles.Input}
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                        placeholder="Password" />
                     <Form.Label className={styles.formLabel}>Password</Form.Label>
                 </Form.Group>
 
                 {/* Password confirmation input */}
                 <Form.Group className="mb-3" controlId="confirm-password">
-                    <Form.Control className={styles.Input} type="password" name="confirm_password" placeholder="Confirm password" />
+                    <Form.Control
+                        className={styles.Input}
+                        type="password"
+                        name="confirm_password"
+                        value={confirm_password}
+                        onChange={handleChange}
+                        placeholder="Confirm password" />
                     <Form.Label className={styles.formLabel}>Confirm password</Form.Label>
                 </Form.Group>
                 {/* <Button variant="primary" type="submit">
@@ -39,4 +76,4 @@ const SingUpForm = () => {
     );
 }
 
-export default SingUpForm
+export default SingUpForm;
