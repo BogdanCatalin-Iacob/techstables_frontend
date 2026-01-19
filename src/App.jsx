46baa38b52677541bdd0c506a343ec8bb3d54f35
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2026-01-19 23:25:39 +0000

    Create Sign in form and add it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './layouts/NavBar/NavBar'
 import { Routes, Route } from 'react-router'
 import './services/api/axiosDefaults'
 import SingUpForm from './pages/auth/SignUp/SingUpForm'
+import SignInForm from './pages/auth/SignIn/SignInForm'
 
 function App() {
     return (
@@ -10,7 +11,7 @@ function App() {
             <NavBar />
             <Routes>
                 <Route exact path="/" element=<h1>Home page</h1> />
-                <Route exact path="signin" element=<h1>Sign in</h1> />
+                <Route exact path="signin" element=<SignInForm /> />
                 <Route exact path="signup" element=<SingUpForm /> />
                 <Route path='*' exact element=<h1>Page not found</h1> />
             </Routes>
