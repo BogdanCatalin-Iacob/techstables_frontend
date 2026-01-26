7f24e9828de1b906bdc679a3066a5cf649b7a476
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2026-01-26 14:55:22 +0000

    Create axios response and request interceptors, and temporarly use a localhost Rest Api

--- src/services/api/axiosDefaults.js
+++ src/services/api/axiosDefaults.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://techstables-2157aa5076c9.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+// axios.defaults.baseURL = 'https://techstables-2157aa5076c9.herokuapp.com/'
+axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
