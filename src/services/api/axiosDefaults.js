9f449e63f248e3e10389b27b2760db450aed6c8b
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2026-02-16 16:27:10 +0000

    Refactor axios interceptors

--- src/services/api/axiosDefaults.js
+++ src/services/api/axiosDefaults.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://techstables-2157aa5076c9.herokuapp.com/'
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
+axios.defaults.baseURL = 'https://techstables-2157aa5076c9.herokuapp.com/';
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
+export const axiosReq = axios.create({
+    baseURL: axios.defaults.baseURL,
+    withCredentials: true,
+});
+export const axiosRes = axios.create({
+    baseURL: axios.defaults.baseURL,
+    withCredentials: true,
+});
