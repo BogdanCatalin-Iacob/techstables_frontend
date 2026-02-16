f7a4d34a0f5b432e1c923d9b6247bf70b2598d3a
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2026-02-16 18:30:27 +0000

    Removed multipart header

--- src/services/api/axiosDefaults.js
+++ src/services/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://techstables-2157aa5076c9.herokuapp.com/';
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create({
